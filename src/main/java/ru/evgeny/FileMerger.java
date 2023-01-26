eaa9929ddb472196053c875a6db562a91869034a
eugenible <ztretyakov@gmail.com>
2023-01-26 15:39:28 +0700

    refactored order of methods in class

--- src/main/java/ru/evgeny/FileMerger.java
+++ src/main/java/ru/evgeny/FileMerger.java
@@ -18,116 +18,6 @@ public class FileMerger {
         this.settings = settings;
     }
 
-    private BufferedReader[] getFileReaders(List<File> inputFiles) {
-        int numOfFiles = inputFiles.size();
-        BufferedReader[] readers = new BufferedReader[numOfFiles];
-        try {
-            for (int i = 0; i < numOfFiles; ++i) {
-                readers[i] = new BufferedReader(new FileReader(inputFiles.get(i)));
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-            closeReaders(readers);
-            readers = null;
-        }
-        return readers;
-    }
-
-    private void closeReaders(BufferedReader[] readers) {
-        for (BufferedReader reader : readers) {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-    }
-
-    private boolean isValid(String line) {
-        if (line.contains(" ")) return false;
-        return settings.getType() != DataType.INTEGER || line.matches("^[+-]?[0-9]+$");
-    }
-
-    // Возвращает true, если current следует за previous в соотв. с порядком order, иначе false.
-    private boolean satisfiesOrder(String current, String previous, Order order) {
-        int currNum, prevNum;
-        if (settings.getType() == DataType.INTEGER) {
-            currNum = Integer.parseInt(current);
-            prevNum = Integer.parseInt(previous);
-            return (order == Order.ASC) ? currNum >= prevNum : currNum <= prevNum;
-        }
-        return (settings.getOrder() == Order.ASC) ? current.compareTo(previous) >= 0 : current.compareTo(previous) <= 0;
-    }
-
-    private void checkReaderDataOrder(String[] inputLines, int index, boolean[] stopReading,
-                                      String[] previousLines) {
-        String prevLine = previousLines[index];
-        String currLine = inputLines[index];
-        if (prevLine == null) {
-            previousLines[index] = currLine;
-            return;
-        }
-
-        if (!satisfiesOrder(currLine, prevLine, settings.getOrder())) {
-            inputLines[index] = null;
-            stopReading[index] = true;
-        } else {
-            previousLines[index] = currLine;
-        }
-    }
-
-    private void fillInputLines(String[] inputLines, BufferedReader[] readers, boolean[] stopReading,
-                                String[] previousLines) throws IOException {
-        for (int i = 0; i < readers.length; ++i) {
-            if (inputLines[i] != null) continue;
-            String line = null;
-            boolean isInvalidLine = true;
-            while (!stopReading[i] && isInvalidLine) {
-                if ((line = readers[i].readLine()) == null) {
-                    stopReading[i] = true;
-                    continue;
-                }
-                if (isValid(line)) {
-                    inputLines[i] = line;
-                    isInvalidLine = false;
-                }
-            }
-
-            if (line != null) checkReaderDataOrder(inputLines, i, stopReading, previousLines);
-        }
-    }
-
-    private String findBestLine(String[] inputLines) {
-        String bestValue = null;
-        int bestElementIndex = -1;
-
-        for (int i = 0; i < inputLines.length; ++i) {
-            if (inputLines[i] == null) continue;
-
-            if (bestValue == null) {
-                bestValue = inputLines[i];
-                bestElementIndex = i;
-            }
-
-            if (satisfiesOrder(inputLines[i], bestValue, settings.getOrder().opposite())) {
-                bestValue = inputLines[i];
-                bestElementIndex = i;
-            }
-        }
-
-        if (bestValue != null) inputLines[bestElementIndex] = null;
-
-        return bestValue;
-    }
-
-    private String getLineToWrite(BufferedReader[] readers, boolean[] stopReading, String[] inputLines,
-                              String[] previousLines) throws IOException {
-        fillInputLines(inputLines, readers, stopReading, previousLines);
-        return findBestLine(inputLines);
-    }
-
     // Алгоритм работы: создается n Reader-ов, где n - кол-во входных файлов, из каждого reader-а считывается одно
     // значение и помещается в массив "отбора", из которого затем выбирается подходящее по условиям сортировки и
     // записывается в выходной файл. В массив "отбора" далее помещается новое значение из соответствующего ридера.
@@ -154,4 +44,111 @@ public class FileMerger {
         closeReaders(readers);
     }
 
+    private String getLineToWrite(BufferedReader[] readers, boolean[] stopReading, String[] inputLines,
+                                  String[] previousLines) throws IOException {
+        fillInputLines(inputLines, readers, stopReading, previousLines);
+        return findBestLine(inputLines);
+    }
+
+    private void fillInputLines(String[] inputLines, BufferedReader[] readers, boolean[] stopReading,
+                                String[] previousLines) throws IOException {
+        for (int i = 0; i < readers.length; ++i) {
+            if (inputLines[i] != null) continue;
+            String line = null;
+            boolean isInvalidLine = true;
+            while (!stopReading[i] && isInvalidLine) {
+                if ((line = readers[i].readLine()) == null) {
+                    stopReading[i] = true;
+                    continue;
+                }
+                if (isValid(line)) {
+                    inputLines[i] = line;
+                    isInvalidLine = false;
+                }
+            }
+
+            if (line != null) checkReaderDataOrder(inputLines, i, stopReading, previousLines);
+        }
+    }
+
+    private void checkReaderDataOrder(String[] inputLines, int index, boolean[] stopReading,
+                                      String[] previousLines) {
+        String prevLine = previousLines[index];
+        String currLine = inputLines[index];
+        if (prevLine == null) {
+            previousLines[index] = currLine;
+            return;
+        }
+
+        if (!satisfiesOrder(currLine, prevLine, settings.getOrder())) {
+            inputLines[index] = null;
+            stopReading[index] = true;
+        } else {
+            previousLines[index] = currLine;
+        }
+    }
+
+    private String findBestLine(String[] inputLines) {
+        String bestValue = null;
+        int bestElementIndex = -1;
+
+        for (int i = 0; i < inputLines.length; ++i) {
+            if (inputLines[i] == null) continue;
+
+            if (bestValue == null) {
+                bestValue = inputLines[i];
+                bestElementIndex = i;
+            }
+            if (satisfiesOrder(inputLines[i], bestValue, settings.getOrder().opposite())) {
+                bestValue = inputLines[i];
+                bestElementIndex = i;
+            }
+        }
+
+        if (bestValue != null) inputLines[bestElementIndex] = null;
+        return bestValue;
+    }
+
+    // Возвращает true, если current следует за previous в соотв. с порядком order, иначе false.
+    private boolean satisfiesOrder(String current, String previous, Order order) {
+        int currNum, prevNum;
+        if (settings.getType() == DataType.INTEGER) {
+            currNum = Integer.parseInt(current);
+            prevNum = Integer.parseInt(previous);
+            return (order == Order.ASC) ? currNum >= prevNum : currNum <= prevNum;
+        }
+        return (settings.getOrder() == Order.ASC) ? current.compareTo(previous) >= 0 : current.compareTo(previous) <= 0;
+    }
+
+    private boolean isValid(String line) {
+        if (line.contains(" ")) return false;
+        return settings.getType() != DataType.INTEGER || line.matches("^[+-]?[0-9]+$");
+    }
+
+    private BufferedReader[] getFileReaders(List<File> inputFiles) {
+        int numOfFiles = inputFiles.size();
+        BufferedReader[] readers = new BufferedReader[numOfFiles];
+        try {
+            for (int i = 0; i < numOfFiles; ++i) {
+                readers[i] = new BufferedReader(new FileReader(inputFiles.get(i)));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            closeReaders(readers);
+            readers = null;
+        }
+        return readers;
+    }
+
+    private void closeReaders(BufferedReader[] readers) {
+        for (BufferedReader reader : readers) {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
 }
