e4258fae0ae5ad5841e6a1d03cf0a49cc295703f
eugenible <ztretyakov@gmail.com>
2023-01-27 15:58:53 +0700

    refactoring files

--- src/main/java/ru/evgeny/Main.java
+++ src/main/java/ru/evgeny/Main.java
@@ -3,25 +3,14 @@ package ru.evgeny;
 import ru.evgeny.merge.FileMerger;
 import ru.evgeny.merge.SortingSettings;
 
-import java.io.File;
 import java.io.IOException;
 
 
 public class Main {
-
-    public static void printInfo(SortingSettings settings) {
-        System.out.println("Order type: " + settings.getOrder());
-        System.out.println("Data type: " + settings.getType());
-        System.out.println("Output file: " + settings.getOutputFile().getName());
-        for (File f : settings.getInputFiles()) {
-            System.out.println("Input file: " + f.getName());
-        }
-    }
-
     public static void main(String[] args) {
         try {
             SortingSettings settings = SortingSettings.getInstance(args);
-            printInfo(settings);
+            SortingSettings.printInfo(settings);
             FileMerger merger = new FileMerger(settings);
             merger.mergeFiles();
         } catch (IOException e) {
--- src/main/java/ru/evgeny/merge/SortingSettings.java
+++ src/main/java/ru/evgeny/merge/SortingSettings.java
@@ -10,12 +10,17 @@ public class SortingSettings {
     private Order order;
     private DataType type;
     private File outputFile;
+    private boolean outputFileFound;
     private List<File> inputFiles;
+    private List<String> missingInputFiles;
 
     public SortingSettings() {
         order = Order.ASC;
         type = DataType.INTEGER;
+        outputFile = null;
+        outputFileFound = false;
         inputFiles = new ArrayList<>();
+        missingInputFiles = new ArrayList<>();
     }
 
     public static SortingSettings getInstance(String[] args) throws IOException {
@@ -36,18 +41,38 @@ public class SortingSettings {
         }
 
         File outputFile = new File(args[++currI]);
-        if (!outputFile.isFile()) outputFile.createNewFile();
+        settings.setOutputFileFound(!outputFile.createNewFile());
         settings.setOutputFile(outputFile);
 
         while (++currI < args.length) {
             File inputFile = new File(args[currI]);
             if (inputFile.isFile()) settings.addInputFile(inputFile);
+            else settings.getMissingInputFiles().add(inputFile.getName());
         }
 
         if (settings.getInputFiles().size() == 0)
             throw new IOException("There must be at least one input file to produce output file");
 
         return settings;
+    }
+
+    public static void printInfo(SortingSettings settings) {
+        System.out.println("Order type: " + settings.getOrder());
+        System.out.println("Data type: " + settings.getType());
+        System.out.println("Output file: " + settings.getOutputFile().getName());
+        for (File f : settings.getInputFiles()) {
+            System.out.println("Input file: " + f.getName());
+        }
+        System.out.println();
+
+        if (!settings.isOutputFileFound()) {
+            System.out.println(
+                    "Выходной файл \"" + settings.getOutputFile().getName() + "\" был найден и был создан программой");
+        }
+
+        for (String name : settings.getMissingInputFiles()) {
+            System.out.println("Входной файл \"" + name + "\" не был найден");
+        }
     }
 
     public void addInputFile(File file) {
@@ -84,6 +109,22 @@ public class SortingSettings {
 
     public void setInputFiles(List<File> inputFiles) {
         this.inputFiles = inputFiles;
+    }
+
+    public boolean isOutputFileFound() {
+        return outputFileFound;
+    }
+
+    public void setOutputFileFound(boolean outputFileFound) {
+        this.outputFileFound = outputFileFound;
+    }
+
+    public List<String> getMissingInputFiles() {
+        return missingInputFiles;
+    }
+
+    public void setMissingInputFiles(List<String> missingInputFiles) {
+        this.missingInputFiles = missingInputFiles;
     }
 }
 
